fd1972b40da94dd6e17498ac56fde4b418bc0a85
Katsunori Nakanishi <n-kats@users.noreply.github.com>
2018-12-01 04:27:16 +0900

    tfhubのモデルの永続化(bat)

--- launch.bat
+++ launch.bat
@@ -3,4 +3,4 @@ set CURRENT=%CURRENT:\=^/%
 set CURRENT=%CURRENT:C:=c^:%
 set CURRENT=%CURRENT:Users/=Users^/"%
 set CURRENT=%CURRENT:/MLN_201812="^/MLN_201812%
-docker run --rm -it -p 8888:8888 -p 6006:6006 -v %CURRENT%:/work/ nharu13/mln201812 jupyter-notebook
+docker run --rm -it -p 8888:8888 -p 6006:6006 -v %CURRENT%:/work/ -v %CURRENT%/tfhub_modules:/tmp/tfhub_modules nharu13/mln201812 jupyter-notebook
